bd24c5b19e0dda26c09e54ad11f04744271861d8
Emi Clark <emiko.clark@gmail.com>
2018-04-14 14:44:00 -0400

    fix layout-b4 adding stackviews

--- View/VideoCell.swift
+++ View/VideoCell.swift
@@ -19,63 +19,28 @@ class VideoCell: BaseCell {
     }
     
 
-//     var video: Video? {
-//         didSet {
+     var videoItemSnippet: Snippet? {
+         didSet {
             
-//             dump(videoItem)
-//             let videoItemSnippet = self.videoItem.snippet
+            if let thumbnailUrlString = videoItemSnippet?.thumbnails?.high?.url {
+                 downloadImage(imageType: "videoThumbnail", urlString: thumbnailUrlString)
+            }
+
+            titleLabel.text = videoItemSnippet?.title
+
+            if let channelSubtitle = videoItemSnippet?.description {
+                 subTitleTextView.text = channelSubtitle
+            }
+
+            if let title = videoItemSnippet?.title {
+                 titleLabel.text = title
+            }
             
-// //            if let profile_image_name  =  videoItem.snippet.. {
-// //                downloadImage(imageType: "profile_image_name", urlString: profile_image_name)
-// ////            }
-            
-//             if let thumbnailUrlString = videoItemSnippet?.thumbnails?.high?.url {
-//                 downloadImage(imageType: "videoThumbnail", urlString: thumbnailUrlString)
-//             }
-
-//             titleLabel.text = videoItemSnippet?.title
-
-//             if let channelSubtitle = videoItemSnippet?.description {
-//                 subTitleTextView.text = channelSubtitle
-//             }
-
-//             // estimate height for titleLabelText
-//             if let title = videoItemSnippet?.title {
-//                 let size = CGSize(width: frame.size.width - 16 - 44 - 8 - 16, height: 1000)
-//                 let options = NSStringDrawingOptions.usesFontLeading.union(.usesLineFragmentOrigin)
-//                 let estimatedRect = NSString(string: title).boundingRect(with: size, options: options, attributes: [ NSAttributedStringKey.font : UIFont.systemFont(ofSize: 17)] , context: nil)
-
-//                 if estimatedRect.size.height > 21 {
-//                     titleLabelHeightConstraint?.constant = 44
-//                 } else {
-//                     titleLabelHeightConstraint?.constant = 21
-//                 }
-//                 print("estimatedRect:", estimatedRect)
-//                 titleLabel.text = title
-//             }
-            
-//             if let channelDescription = videoItemSnippet?.description {
-//                 let size = CGSize(width: frame.size.width - 16 - 44 - 8 - 16, height: 1000)
-//                 let options = NSStringDrawingOptions.usesFontLeading.union(.usesLineFragmentOrigin)
-//                 let estimatedRect = NSString(string: channelDescription).boundingRect(with: size, options: options, attributes: [ NSAttributedStringKey.font : UIFont.systemFont(ofSize: 17)] , context: nil)
-                
-//                 if estimatedRect.size.height > 21 {
-//                     titleLabelHeightConstraint?.constant = 44
-//                 } else {
-//                     titleLabelHeightConstraint?.constant = 21
-//                 }
-//                 print("estimatedRect:", estimatedRect)
-//                 subTitleTextView.text = channelDescription
-//             }
-
-//             // estimate height for subTitle
-//             // estimate height for VideoCell (video+16+titleLabelHeight+8+subTitleLabelHeight+16)
-            
-//             if let thumbnailImageUrlString = videoItemSnippet?.thumbnails?.high?.url {
-//                 downloadImage(imageType: "thumbnail", urlString: thumbnailImageUrlString)
-//             }
-//         }
-//     }
+            if let channelDescription = videoItemSnippet?.description {
+                 subTitleTextView.text = channelDescription
+            }
+         }
+     }
     
     let thumbnailImageView: UIImageView = {
         let imageView = UIImageView()
@@ -85,7 +50,7 @@ class VideoCell: BaseCell {
         imageView.layer.borderColor = UIColor( red: 0.8, green: 0.8, blue: 0.8, alpha: 1.0 ).cgColor
         imageView.clipsToBounds = true
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.image = #imageLiteral(resourceName: "dragon")
+        imageView.image = #imageLiteral(resourceName: "dragonPlaceholder")
         return imageView
     }()
     
@@ -95,7 +60,7 @@ class VideoCell: BaseCell {
         imageView.layer.masksToBounds = true
         imageView.contentMode = .scaleAspectFit
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.image = #imageLiteral(resourceName: "channelDragonPlaceholder")
+        imageView.image = #imageLiteral(resourceName: "dragon")
         return imageView
     }()
     
@@ -163,7 +128,6 @@ class VideoCell: BaseCell {
             titleLabel.leadingAnchor.constraint(equalTo: profileImageView.leadingAnchor, constant: 50),
             titleLabel.trailingAnchor.constraint(equalTo: thumbnailImageView.trailingAnchor, constant: 0),
             titleLabel.bottomAnchor.constraint(equalTo: subTitleTextView.topAnchor, constant: 8)
-//            titleLabel.heightAnchor.constraint(equalTo: self.heightAnchor, constant: -20)
         ])
         
         // subtitle constraints
@@ -201,7 +165,7 @@ class VideoCell: BaseCell {
                     }
                     self.delegate?.updateUI()
                 }
-                }.resume()
+            }.resume()
         }
     }
 
